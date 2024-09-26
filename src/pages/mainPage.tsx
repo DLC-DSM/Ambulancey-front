ab26721dd9b497f53b6ab5acd86732efe68294c2
SeoJiYu <jihoseo2008@gmail.com>
2024-09-26 11:48:22 +0900

    add main

---
+++ src/pages/mainPage.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+import { Colors } from "../Style/colors"
+
+function MainPage() {
+    return (
+        <>
+            <Background></Background>
+        </>
+    )
+}
+
+export default MainPage
+
+const Background = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
