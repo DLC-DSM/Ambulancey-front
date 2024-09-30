5e2d3253fe05f45a84995176c79e1831b7643ede
SeoJiYu <jihoseo2008@gmail.com>
2024-09-30 18:15:36 +0900

    StarLeveling component

---
+++ src/components/StarLeveling.tsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react"
+import styled from "styled-components"
+import { Colors } from "../Style/colors"
+
+interface prop {
+    exp: number
+}
+
+function StarLeveling({ exp }: prop) {
+    const EXP = exp * 20
+
+    return (
+        <Border>
+            <Range width={EXP} />
+        </Border>
+    )
+}
+
+export default StarLeveling
+
+const Border = styled.div`
+    width: 300px;
+    height: 12px;
+    border: 1px solid ${Colors.Blue500};
+    border-radius: 6px;
+    display: flex;
+    justify-content: start;
+    align-items: center;
+`
+
+const Range = styled.div`
+    width: ${({ width }) => `${width}%`};
+    height: 12px;
+    border-radius: 4px;
+    background: linear-gradient(to right, ${Colors.Blue500}, ${Colors.Blue300});
+`
